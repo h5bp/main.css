3db11b0f9a31c3a5896056fdeca6e0ec8c2c8e56
Rob Larsen <rob@htmlcssjavascript.com>
2018-09-21 14:24:01 -0400

    Breaks main.css into component parts

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,24 +1,34 @@
 import gulp from 'gulp';
 import pkg from './package.json';
-import replace from 'gulp-replace';
 import header from 'gulp-header';
-import fs from 'fs';
+import cssimport from 'gulp-cssimport';
+import del from 'del';
 
 const license = `/* ${pkg.name} ${pkg.version} | ${pkg.license} License | ${pkg.homepage} */\n`;
 
 const src = './src/css/';
+const dist = './dist/';
+const main = 'main.css';
+let files = [
+  './src/css/_base.css',
+  './src/css/_helpers.css',
+  './src/css/_mqs.css',
+  './src/css/_print.css' ];
 
-let files;
 
-fs.readdir(src, (err, dir) =>{
-  files = dir;
-});
-
-
-gulp.task('main', () => {
-  return gulp.src(files)
+gulp.task('concat', () => {
+  gulp.src(src + main)
+    .pipe(cssimport())
     .pipe(header(license))
-    .pipe(gulp.dest('dist'));
+    .pipe(gulp.dest(dist));
+});
+gulp.task('copy', () => {
+  gulp.src(files,{})
+    .pipe(header(license))
+    .pipe(gulp.dest(dist))
+});
+gulp.task('clean', () => {
+  del([dist]);
 });
 
-gulp.task('default',['main']);
+gulp.task('default',['copy','concat']);
