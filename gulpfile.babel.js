ddd8e50c1b80a108a0552b359c1e717dad484d3b
Rob Larsen <rob@drunkenfist.com>
2018-10-03 22:32:11 -0400

    Break main.css into multiple files (#5)
    This commit breaks the main.css file up into multiple files and builds
    it out to a ./dist/ folder

---
+++ gulpfile.babel.js
@@ -0,0 +1,40 @@
+import gulp from 'gulp';
+import pkg from './package.json';
+import header from 'gulp-header';
+import cssimport from 'gulp-cssimport';
+import del from 'del';
+import plugins from 'gulp-load-plugins';
+
+const license = `/* ${pkg.name} ${pkg.version} | ${pkg.license} License | ${pkg.homepage} */\n`;
+
+const src = './src/';
+const dist = './dist/';
+const main = 'main.css';
+let files = [
+  `${src}/_base.css`,
+  `${src}/_helpers.css`,
+  `${src}/_mqs.css`,
+  `${src}/_print.css`
+];
+
+
+gulp.task('concat', () => {
+  gulp.src(src + main)
+    .pipe(cssimport())
+    .pipe(header(license))
+    .pipe(plugins().autoprefixer({
+      browsers: ['last 2 versions', 'ie >= 11', '> 1%'],
+      cascade: false
+    }))
+    .pipe(gulp.dest(dist));
+});
+gulp.task('copy', () => {
+  gulp.src(files,{})
+    .pipe(header(license))
+    .pipe(gulp.dest(dist));
+});
+gulp.task('clean', () => {
+  del([dist]);
+});
+
+gulp.task('default',['copy','concat']);
